2843272bd86a606cdcd5f00f0df56e006c53a98b
juliwalter <julianwalter1593@gmail.com>
2023-12-19 19:50:01 +0100

    Added a settings file for tests

--- olxcrawler/olxcrawler/settings/test.py
+++ olxcrawler/olxcrawler/settings/test.py
@@ -1,5 +1,4 @@
 from olxcrawler.settings.base import *
-import os
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-ox0e$_crf^($-&@%42u_@wbqtq9j6cyh#o=c5eh0p!o=mt*r(9"
@@ -10,17 +9,12 @@ DEBUG = True
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "olxcrawler",
-        "USER": "dev",
-        "PASSWORD": "dev",
-        "HOST": os.uname().nodename,
-        "PORT": "3306",
-    },
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'test-database',
+    }
 }
 
 # django-apscheduler settings
 # https://apscheduler.readthedocs.io/en/3.x/userguide.html
-
-SCHEDULER_DEFAULT = True
+SCHEDULER_DEFAULT = False
